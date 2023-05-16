0100229a9076b25415874771791806c286978391
rajahwu <vmradford@gmail.com>
2023-05-16 18:47:15 -0400

    add create a club form

--- react-app/src/pages/CreatePage/index.jsx
+++ react-app/src/pages/CreatePage/index.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import CreatePageCSS from "./CreatePage.module.css";
 
 function createPageTitle(location) {
   let title;
@@ -18,10 +20,46 @@ function createPageTitle(location) {
 export default function CreatePage() {
   const location = useLocation();
   const title = createPageTitle(location);
+  const [clubName, setClubName] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   return (
     <>
       <h1>{title}</h1>
+      <form
+        className={CreatePageCSS.createForm}
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = {
+            clubName,
+            description,
+            imageUrl,
+          };
+          console.log(formData);
+        }}
+      >
+        <label>Club Image</label>
+        <input
+          type="text"
+          name="imageUrl"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <label>Club Name</label>
+        <input
+          name="name"
+          type="text"
+          value={clubName}
+          onChange={(e) => setClubName(e.target.value)}
+        />
+        <label>Club Description</label>
+        <textarea
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </form>
       <Link to="/feed">Back to Feed</Link>
     </>
   );
