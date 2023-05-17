8c9e3761154c222f46ea1d12defa769a994d8c46
rajahwu <vmradford@gmail.com>
2023-05-16 22:07:17 -0400

    render all clubs on feed page

--- react-app/src/pages/FeedPage/index.jsx
+++ react-app/src/pages/FeedPage/index.jsx
@@ -1,35 +1,42 @@
-import { useSelector, useDispatch } from "react-redux"
-import { useEffect } from "react"
-import { useHistory } from "react-router-dom"
-import { getAllClubsThunk } from "../../store/club"
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { getAllClubsThunk } from "../../store/club";
 
 export default function FeedPage() {
-    const dispatch = useDispatch()
-    const sessionUser = useSelector(state => state.session.user)
-    const groups = useSelector(state => state.club?.allClubs)
-    const history = useHistory()
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const clubs = useSelector((state) => state.clubs.allClubs);
+  const history = useHistory();
 
-    useEffect(() => {
-        if (!sessionUser) {
-            console.log("splash page session user", sessionUser)
-            return history.push("/")
-        }
-    }, [sessionUser])
+  useEffect(() => {
+    if (!sessionUser) {
+      console.log("splash page session user", sessionUser);
+      return history.push("/");
+    }
+  }, [sessionUser]);
 
+  useEffect(() => {
+    // fetch("/api/clubs")
+    // .then(data => data.json())
+    // .then(data => console.log(data))
+    dispatch(getAllClubsThunk());
+    console.log("feed clubs value: ", clubs);
+  }, []);
 
-    useEffect(() => {
-        // fetch("/api/clubs")
-        // .then(data => data.json())
-        // .then(data => console.log(data))
-        dispatch(getAllClubsThunk())
-
-    }, [])
-
-    return (
+  return (
     <>
-    <h1>Feed Page</h1>
-    <button onClick={() => history.push("/clubs/new")}>Start a Group</button>
-    <button style={{cursor: "not-allowed"}}>Build a Stand</button>
+      <h1>Feed Page</h1>
+      <button onClick={() => history.push("/clubs/new")}>Start a Group</button>
+      <button style={{ cursor: "not-allowed" }}>Build a Stand</button>
+      <div>
+        {clubs && Object.values(clubs).map((club, index) => (
+          <div key={index}>
+            <p>{club.id}</p>
+            <p>{club.name}</p>
+          </div>
+        ))}
+      </div>
     </>
-    )
+  );
 }
