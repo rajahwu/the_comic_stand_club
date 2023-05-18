ba8bf9552d1b6716bf98966b682fc2350fd29938
rajahwu <vmradford@gmail.com>
2023-05-18 14:03:59 -0400

    remove undo membershps from seed all

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -18,7 +18,7 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_memberships()
+        # undo_memberships()
         undo_clubs()
         undo_users()
     seed_users()
