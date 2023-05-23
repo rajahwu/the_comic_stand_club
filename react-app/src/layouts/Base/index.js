c1413effaafe91ace624b1abb2823dd12d1ff51c
rajahwu <vmradford@gmail.com>
2023-05-22 23:59:44 -0400

    complete base css

--- react-app/src/layouts/Base/index.js
+++ react-app/src/layouts/Base/index.js
@@ -1,17 +1,17 @@
 import { Navigation } from "../../components";
-import BaseLayoutCSS from "./BaseLayout.module.css"
+import BaseLayoutCSS from "./BaseLayout.module.css";
 
 export default function BaseLayout({ isLoaded, children }) {
   return (
-    <>
-      <header className={BaseLayoutCSS.container}>
+    <div className={BaseLayoutCSS.container}>
+      <header className={BaseLayoutCSS.header}>
         <Navigation isLoaded={isLoaded} />
         <h1>The Comic Stand Club</h1>
       </header>
-      <div>{children}</div>
-      <footer className={BaseLayoutCSS.container}>
+      <div style={{ pointerEvents: "auto" }}>{children}</div>
+      <footer>
         <h2>Footer</h2>
       </footer>
-    </>
+    </div>
   );
 }
