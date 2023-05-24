19f1945d73b43795714d69947596f5acaa115b9b
rajahwu <vmradford@gmail.com>
2023-05-23 22:39:34 -0400

    add button hover effects

--- react-app/src/components/FeedComponents/FeedButtons/index.jsx
+++ react-app/src/components/FeedComponents/FeedButtons/index.jsx
@@ -24,17 +24,36 @@ export default function FeedButtons() {
     height: "2.5rem",
     width: "5rem",
     margin: 0,
-    color: "white",
-    backgroundColor: "black",
+    color: "#d2b811",
+    backgroundColor: " hsl(240, 5%, 4%)",
     cursor: "pointer",
-  }
+    opacity: 0.8
+  };
+
+  const handleMouseEnter = (e) => {
+    console.log(e.target.style)
+    console.log("height", e.target.style["height"], "boxShadow", e.target.style["width"])
+    e.target.style.color = "#f2eadf";
+    e.target.style["box-shadow"] = "2px 2px 2px hsl(120, 64%, 17%)";
+    e.target.style["height"] = "2.7rem";
+    e.target.style["width"] = "5.3rem";
+    e.target.style["opacity"] = 1
+  };
+  const handleMouseLeave = (e) => {
+    e.target.style.color = "#d2b811";
+    e.target.style.border = "1px solid #fee52e";
+    e.target.style["box-shadow"] = "";
+    e.target.style["height"] = "2.5rem";
+    e.target.style["width"] = "5rem";
+    e.target.style["opacity"] = 0.8;
+  };
 
   return (
     <div className="clubs-feed">
-    <Link to={`/feed/${activeFeed.type}s`}>
-      <h2>My {activeFeed.type}s</h2>
-    </Link>
-      <div style={{display: "flex", justifyContent:"space-around"}}>
+      <Link to={`/feed/${activeFeed.type}s`}>
+        <h2 className={FeedButtonsCSS["title"]}>My {activeFeed.type}s</h2>
+      </Link>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
         <button
           className={FeedButtonsCSS["feed-btn"]}
           style={{
@@ -44,6 +63,8 @@ export default function FeedButtons() {
                 ? "1px solid yellow"
                 : "1px solid white",
           }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           onClick={() => {
             if (activeFeed.type === "club") history.push("/feed/clubs");
             setActiveFeed({ type: "club", url: "/clubs" });
@@ -56,11 +77,14 @@ export default function FeedButtons() {
           className={FeedButtonsCSS["feed-btn"]}
           style={{
             ...btnStyles,
+           
             border:
               activeFeed.type === "stand"
                 ? "1px solid yellow"
                 : "1px solid white",
           }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           onClick={() => {
             if (activeFeed.type === "stand") history.push("/feed/stands");
             setActiveFeed({ type: "stand", url: "/stands" });
@@ -86,9 +110,11 @@ export default function FeedButtons() {
         <button
           className={FeedButtonsCSS["feed-btn"]}
           style={{
-           ...btnStyles,
-            border: "1px solid green",
+            ...btnStyles,
+            border: "1px solid #d2b811",
           }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           onClick={() => history.push(`${activeFeed.url}-new`)}
         >
           {activeFeed.type === "stand" ? "Create New Stand" : "Create New Club"}
